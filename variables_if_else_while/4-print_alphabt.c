888af36e9ec6364784f9283f598ede333c1cf977
SilviMarku <marku.silvia777@gmail.com>
2023-06-19 06:29:04 -0700

    Taskt 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
