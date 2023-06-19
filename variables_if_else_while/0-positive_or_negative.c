c78e48f66fa4cf37008c5c95100167658c4bb46c
SilviMarku <marku.silvia777@gmail.com>
2023-06-19 02:54:06 -0700

    Task 1

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (
+	return (0);
+}
