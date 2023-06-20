64a5f1634b9093a95c20c15068479308ba2dceab
SilviMarku <marku.silvia777@gmail.com>
2023-06-20 07:00:47 -0700

    Task 8

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - it takes an argument of
+ * type int and returns its last digit
+ * @n: integer
+ * Return: the last digit of the number
+ */
+
+
+int print_last_digit(int n)
+{
+if (n < 0)
+{
+n = n % 10;
+n = n * -1;
+_putchar('0' + n);
+return (n);
+}
+else
+{
+n = n % 10;
+_putchar('0' + n);
+return (n);
+}
+}
