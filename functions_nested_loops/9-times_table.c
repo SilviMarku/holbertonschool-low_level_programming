a04f6857c2aa2a54e6f6b6c4c487f8ced4efc9b1
SilviMarku <marku.silvia777@gmail.com>
2023-06-21 01:40:21 -0700

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,15 +5,15 @@
 void times_table(void)
 {
 int i, j;
-int sum;
+int result;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-sum = i * j;
-if (sum >= 10)
-_putchar('0' + sum / 10);
-_putchar('0' + sum % 10);
+result = i * j;
+if (result >= 10)
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
 if (j < 9)
 {
 _putchar(',');
