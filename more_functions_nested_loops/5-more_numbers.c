a236d36af8da81360cdd37e3464b2524e2d5130f
SilviMarku <marku.silvia777@gmail.com>
2023-06-22 06:00:19 -0700

    Taskk 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * more_numbers - main function
+ */
+
+void more_numbers(void)
+{
+	int count;
+	int i;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (i = 0;  i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + '0');
+			_putchar ((i % 10) + '0');
+		}
+	}
+	_putchar ('\n');
+}
