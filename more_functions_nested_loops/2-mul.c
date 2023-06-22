2e6548c9042b2e721fa18d7d6b1d66765ecf2188
SilviMarku <marku.silvia777@gmail.com>
2023-06-22 04:34:48 -0700

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * mul - main function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 1 if c is a digit and 0 if otherwise
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
