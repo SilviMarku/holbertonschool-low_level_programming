5acc6c40e48d361d2c2711a97527cdd403f7bbaf
SilviMarku <marku.silvia777@gmail.com>
2023-06-23 03:42:20 -0700

    Task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,14 +2,23 @@
 #include "main.h"
 
 /**
- * print_line - main function
- * @n: is the number of times the character _ should be printed
+ * print_triangle - main function
+ * @size: parameter
+ * Return: 1 if uppercase and 0 if false
  */
-void print_line(int n)
-{
-	int i;
 
-	for (i = 1; i <= n; i++)
-		_putchar('_');
-	_putchar('\n');
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j > 0; j--)
+			_putchar(' ');
+		for (j = 0; j < i; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
