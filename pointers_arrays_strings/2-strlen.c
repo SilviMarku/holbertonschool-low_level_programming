3dd2bb69d88f45e2f1bbc7cba9f74df3f6437e4c
SilviMarku <marku.silvia777@gmail.com>
2023-06-26 03:34:40 -0700

    Pointers,arrays and strings

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Write a function that returns the length of a string
+ *
+ * @n: pointer to the variable to apdate
+ */
+
+int _strlen(char *s)
+{
+    int l = 0;
+    while(s[i] != '\0')
+	    l++;
+    return (l);
+}	
