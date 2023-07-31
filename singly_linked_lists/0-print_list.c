864dec8a7ef9fc813d93dc92cb0585729bb7b5c2
SilviMarku <marku.silvia777@gmail.com>
2023-07-31 07:25:49 -0700

    Function that prints all the elements

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - function that prints all the elements
+ *@h: header node
+ *@n: counter
+ *Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (n);
+}
---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+
+
+list_t *add_node(list_t **head, const char *str);
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL || str == NULL)
+	{
+		free(new_node->str);
+		free(new_node);;
+		return NULL;
+	}
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
