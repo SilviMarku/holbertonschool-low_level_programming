3d13e236d2edbddd83b70ef1e385244bc3ad2702
SilviMarku <marku.silvia777@gmail.com>
2023-08-02 05:13:44 -0700

    F#include <stdlib> #include list.h #include <string.h> #include <stdio.h> /**  * add_dnodeint_end - function that adds a new node at the end  *@head: first node  *@n: value to the new node  *Return: the address of the new element  */
    dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
    {
            dlistint_t *new_end,*tmp;
    
            new_end = malloc(sizeof(dlistint_t));
            if (new_end == NULL)
                    return (NULL);
    
            new_end->n = n;
            new_end->next = NULL;
    
            tmp = *head;
    
            if (tmp == NULL)
            {
                    *head = new_end;
                    return (new_end);
            }
            while (tmp->prev != NULL)
            {
                    tmp = tmp->next;
            }
            new_end->prev = tmp;
            tmp->next = new_end;
    
            return (new_end);
                                                                                                                  6,17          Top
    vi 7-insert_dnodeint.c clear
    cd ..
    vi 3-

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,40 @@
+#include <stdlib>
+#include "list.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * add_dnodeint_end - function that adds a new node at the end
+ *@head: first node
+ *@n: value to the new node
+ *Return: the address of the new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+        dlistint_t *new_end,*tmp;
+
+        new_end = malloc(sizeof(dlistint_t));
+        if (new_end == NULL)
+                return (NULL);
+
+        new_end->n = n;
+        new_end->next = NULL;
+
+        tmp = *head;
+
+        if (tmp == NULL)
+        {
+                *head = new_end;
+                return (new_end);
+        }
+        while (tmp->prev != NULL)
+        {
+                tmp = tmp->next;
+        }
+        new_end->prev = tmp;
+        tmp->next = new_end;
+
+        return (new_end);
+                                                                                                              6,17          Top
+}
