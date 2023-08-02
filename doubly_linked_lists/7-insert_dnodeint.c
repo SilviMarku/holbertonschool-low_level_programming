8d0f50a9e98c72fdf05f621268d990196f3fd129
SilviMarku <marku.silvia777@gmail.com>
2023-08-02 04:13:36 -0700

    function that inserts a new node at a given position.
    cd ..
    
    cd ..
    
    c
    
    '

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,45 +1,44 @@
-#include <stdlib>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
-#include <stddef>
-#include <stdio>
-
 /**
- *insert_dnodeint_at_index - funcion that inserts a new node
- *
- *
- *@n: valeu of new node 
- *
- * /
- 
+ * insert_dnodeint_at_index - function that inserts a new node at idx position
+ * @h: first node
+ * @idx: index where the node should be added
+ * @n: value to add to the new node
+ * Return: adress of the new node or NULL
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{ 
-	dlistint_t *new, *tmp;
+{
+	dlistint_t *new_n, *tmp;
 	unsigned int count;
-	
-	new = malloc(sizeof(dlistint_t));
-	
-	if (new == NULL);
-	return NULL;
-	new-> = n;
-	
-	if (inx == 0)
-	return add_dnodeint(h,n);
-	
+
+	new_n = malloc(sizeof(dlistint_t));
 	tmp = *h;
-	for (count = 0, count < inx -1, count++)
 
-	if (tmp == NULL)
-	return NULL;
+	if (new_n == NULL)
+		return (NULL);
+	new_n->n = n;
 
-	tmp = tmp->next;
+	if (idx == 0 || (*h) == NULL)
+		return (add_dnodeint(h, n));
+	{
+		for (count = 0; count < idx - 1; count++)
+		{
+			if (tmp->next == NULL && count < idx)
+				return (NULL);
 
-	if (tmp->next == NULL)
-	return add_dnodeint_end(h, n);
+			tmp = tmp->next;
+		}
+		if (tmp->next == NULL)
+			return (add_dnodeint_end(h, n));
 
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next ->prev =new;
-	tmp->next = new;
-
-	return new;
+		new_n->next = tmp->next;
+		new_n->prev = tmp;
+		tmp->next->prev = new_n;
+		tmp->next = new_n;
+		return (new_n);
+	}
+	return (NULL);
 }
